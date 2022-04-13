713cb2e0f630b79be2d52382d72e755c432f25ac
Tiago Cesar da Silva Lopes <tiagolopes@pe.senac.br>
2022-04-13 09:55:43 -0300

    Criação da classe java Aluno

---
+++ Aluno.java
@@ -0,0 +1,59 @@
+public class Aluno{
+    private String nome;
+    private String sexo;
+    private String matricula;
+    private String cpf;
+    private String identidade;
+    private String curso;
+
+    public Aluno(){
+
+    }
+
+    public String getNome(){
+        return this.nome;
+    }
+
+    public String getSexo(){
+        return this.sexo;
+    }
+
+    public String getMatricula(){
+        return this.matricula;
+    }
+
+    public String getCpf(){
+        return this.cpf;
+    }
+
+    public String getIdentidade(){
+        return this.identidade;
+    }
+
+    public String getCurso(){
+        return this.curso;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public void setSexo(String sexo){
+        this.sexo = sexo;
+    }
+
+    public void setMatricula(String matricula){
+        this.matricula = matricula;
+    }
+
+    public void setCpf(String cpf){
+        this.cpf = cpf;
+    }
+    public void setIdentidade(String identidade){
+        this.identidade = identidade;
+    }
+
+    public void setCurso (String curso){
+    this.curso = curso;
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # 3--associa-o-de-classes-java-Refeito
 Este repositório será para alocar o projeto de associação de classes refeito
+
+
+Olá! Estou refazendo um projeto de repositório já existente, que se encontra neste link: https://github.com/TiagoUniverse/exercicio-3-associa-cao-de-classses-em-java .
+
+O código do antigo repositório começou a apresentar um bug, então achei melhor debugá-lo de passo à passo, colocando os recursos por etapa. Assim que terminar, eu atualizarei também o outro repositório com o código corrigido.
+
+Qualquer coisa pode entrar em contato comigo.
+
+Atenciosamente,
+
+Tiago César.
